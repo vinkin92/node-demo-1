2ef997418419c92108463fec70a0b7f02e15209e
vinkin <928287706@qq.com>
2020-12-23 20:12:57 +0800

    node.js服务器测试

---
+++ server.js
@@ -0,0 +1,58 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.argv[2]
+
+if (!port) {
+    console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+    process.exit(1)
+}
+
+var server = http.createServer(function(request, response) {
+    var parsedUrl = url.parse(request.url, true)
+    var pathWithQuery = request.url
+    var queryString = ''
+    if (pathWithQuery.indexOf('?') >= 0) { queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+    var path = parsedUrl.pathname
+    var query = parsedUrl.query
+    var method = request.method
+
+    /******** 从这里开始看，上面不要看 ************/
+
+
+    if (path === '/') {
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        response.write(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Node.js 服务器</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <h1>服务器连接成功</h1>
+    </body>
+    </html>
+    `)
+        response.end()
+    } else if (path === '/style.css') {
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/css;charset=utf-8')
+        response.write(`h1{color: red;}`)
+        response.end()
+    } else {
+        response.statusCode = 404
+        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        response.write(`你访问的页面不存在`)
+        response.end()
+    }
+
+    /******** 代码结束，下面不要看 ************/
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功\n访问地址： http://localhost:' + port)
